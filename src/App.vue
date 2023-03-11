7d854be47aea2fda92faea52821be5a0e8e8a56d
Hooorus <2479711422@qq.com>
2023-03-11 19:14:52 +0800

    基本完成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,207 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>Welcome to RankPage Front</span>
+    <br/>
+    <br/>
+    <span>当前标题名称: {{ this.current_title }}</span>
+    <a-divider>评分区</a-divider>
+    <a-button type="primary" @click="queryResultByTrack">
+      刷新数据
+    </a-button>
+    <a-table :data-source="tableData" :columns="columns">
+      <div
+          slot="filterDropdown"
+          slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
+          style="padding: 8px"
+      >
+        <a-input
+            v-ant-ref="c => (searchInput = c)"
+            :placeholder="`Search ${column.dataIndex}`"
+            :value="selectedKeys[0]"
+            style="width: 188px; margin-bottom: 8px; display: block;"
+            @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+            @pressEnter="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
+        />
+        <a-button
+            type="primary"
+            icon="search"
+            size="small"
+            style="width: 90px; margin-right: 8px"
+            @click="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
+        >
+          Search
+        </a-button>
+        <a-button size="small" style="width: 90px" @click="() => handleReset(clearFilters)">
+          Reset
+        </a-button>
+      </div>
+      <a-icon
+          slot="filterIcon"
+          slot-scope="filtered"
+          type="search"
+          :style="{ color: filtered ? '#108ee9' : undefined }"
+      />
+      <template slot="customRender" slot-scope="text, record, index, column">
+      <span v-if="searchText && searchedColumn === column.dataIndex">
+        <template
+            v-for="(fragment, i) in text
+            .toString()
+            .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
+        >
+          <mark
+              v-if="fragment.toLowerCase() === searchText.toLowerCase()"
+              :key="i"
+              class="highlight"
+          >{{ fragment }}</mark
+          >
+          <template v-else>{{ fragment }}</template>
+        </template>
+      </span>
+        <template v-else>
+          {{ text }}
+        </template>
+      </template>
+    </a-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      current_title: "",
+
+      tableData: [],
+      searchText: '',
+      searchInput: null,
+      searchedColumn: '',
+      columns: [
+        {
+          title: '名字  ',
+          dataIndex: 'name',
+          key: 'name',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.name
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              }, 0);
+            }
+          },
+        },
+        {
+          title: '得分',
+          dataIndex: 'score',
+          key: 'score',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.score
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        },
+        {
+          title: '赛道',
+          dataIndex: 'track',
+          key: 'track',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+              record.track
+                  .toString()
+                  .toLowerCase()
+                  .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              });
+            }
+          },
+        }]
+    }
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    //数据查询
+    async queryResultByTrack() {
+      const formData = new FormData();
+      console.log(this.current_title);
+      var str = this.current_title;
+      formData.append('track', str)
+      await Axios.request({
+        url: "http://49.235.113.96:8099/rank_page/front/get_people_by_track",
+        method: 'POST',
+        data: formData,
+      }).then(res => {
+        console.log("str: " + str)
+        this.tableData = res.data.data
+        console.log(res.data)
+        //结果集处理
+        if (res.data.status != 200) {
+          this.$message.error('查询失败！');
+        }else {
+          this.$message.success("查询成功！")
+        }
+      })
+    },
+
+    //表格方法
+    handleSearch(selectedKeys, confirm, dataIndex) {
+      confirm();
+      this.searchText = selectedKeys[0];
+      this.searchedColumn = dataIndex;
+    },
+
+    handleReset(clearFilters) {
+      clearFilters();
+      this.searchText = '';
+    },
+
+    //初始化查询标题
+    async init() {
+      //组装参数
+      let requestParam = new FormData()
+      requestParam.append("title_id", "title_name")
+      await Axios.request({
+        method: 'GET',
+        url: 'http://49.235.113.96:8099/rank_page/front/get_title_name?title_id=title_name',
+        data: requestParam,
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }).then(res => {
+        console.log('res.data=>', res.data.data);
+        this.current_title = res.data.data
+      })
+    },
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+import {Button, message} from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import Axios from "axios";
 
 Vue.config.productionTip = false
 
+Vue.use(Axios)
+Vue.use(Antd);
+Vue.use(Button);
+Vue.prototype.$message = message;
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
